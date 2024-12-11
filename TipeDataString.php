0de1283af63c07b2151316acf3f8caa10ebfebba
Lorens Ryan Hinaweni <168622005+Crowy24@users.noreply.github.com>
2024-12-11 18:11:38 +0800

    Add files via upload

---
+++ TipeDataString.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "Name : ";
+echo "Eko Kurniawan Khannedy";
+echo "\n";
+
+echo "Name : ";
+echo "Eko\t Kurniawan\t Khannedy\n";
+
+echo <<<RYAN
+Ini adalah contoh string yang sangat panjang, dan juga gak perlu ngetik ENTER secara manual, "bisa quote" juga
+RYAN;
+echo <<<'RYAN'
+Ini adalah contoh string yang sangat panjang, dan juga gak perlu ngetik ENTER secara manual, "bisa quote" juga
+RYAN;
---
+++ Variable.php
@@ -0,0 +1,12 @@
+<?php
+
+$name = "Eko";
+$age = 27;
+
+echo "Name :";
+echo $name;
+echo "\n";
+
+echo "Age : ";
+echo $age;
+echo "\n";
